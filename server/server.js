2f28f3fdecac7d5b5a9f09bceefe16d398af53dc
Aaditya <ad2340033@gmail.com>
2025-07-23 21:24:30 +0530

    health check api added

--- server/server.js
+++ server/server.js
@@ -23,6 +23,11 @@ import blogRoutes from './routes/blogs.routes.js';
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/blogs', blogRoutes);
 
+// Render health check
+app.get('/api/health', (req, res) => {
+    res.status(200).json({ status: 'OK', timestamp: new Date() });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
