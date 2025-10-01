0591cbc608d98d90d766f4e24afbb917ae5a109c
Robin Bisht <130886823+robinbishtt@users.noreply.github.com>
2025-10-01 11:49:41 +0530

    Revert "Create aggregation code.js"

--- aggregation code.js
+++
@@ -1,6 +0,0 @@
-const totalLikesAgg = await Post.aggregate([
-  { $project: { likesCount: { $size: { $ifNull: ["$likes", []] } } } },
-  { $group: { _id: null, total: { $sum: "$likesCount" } } }
-]);
-const totalLikes = totalLikesAgg[0]?.total || 0;
-res.json({ topPosts, totalPosts, totalLikes });
